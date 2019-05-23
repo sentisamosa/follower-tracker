f34c7c44f5e5c9baba30aaa7584f3db753056e90
Kshitij Saxena <kshitij.a.saxena@accenture.com>
2019-05-23 12:26:36 +0530

    tracker service added

--- app/src/main/java/com/mr/j/TrackerService.java
+++ app/src/main/java/com/mr/j/TrackerService.java
@@ -12,4 +12,14 @@ public class TrackerService extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        return START_STICKY;
+    }
 }
