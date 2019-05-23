58b8d86837d0c409cb34b4fb5c400c55d5c2af62
Kshitij Saxena <kshitij.a.saxena@accenture.com>
2019-05-23 18:00:40 +0530

    ui changes

--- app/src/main/java/com/mr/j/TrackerService.java
+++ app/src/main/java/com/mr/j/TrackerService.java
@@ -7,11 +7,14 @@ public class TrackerService extends JobService {
 
     @Override
     public boolean onStartJob(JobParameters params) {
+        //Todo do everything that the service was supposed to do.
+        //Todo use this link https://blog.teamtreehouse.com/scheduling-work-jobscheduler
         return false;
     }
 
     @Override
     public boolean onStopJob(JobParameters params) {
+        //Todo Think how to use this method. This method will be called by the Android system when the job is stopped in the mid.
         return false;
     }
 }
