cb4a8f9eb566c7a036eca8e2054757413cd66c8e
Kshitij Saxena <kshitij.a.saxena@accenture.com>
2019-05-24 12:44:13 +0530

    project updated

--- app/src/main/java/com/mr/j/MainActivity.java
+++ app/src/main/java/com/mr/j/MainActivity.java
@@ -1,17 +1,40 @@
 package com.mr.j;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
+    JobScheduler jobScheduler;
+
+    private Switch.OnCheckedChangeListener toggleJob = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if (isChecked) {
+                int resultCode = jobScheduler != null ? jobScheduler.schedule(getJobInfo()) : 0;
+                if (resultCode == JobScheduler.RESULT_SUCCESS) {
+                    Toast.makeText(MainActivity.this, "Tracker Started", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Error starting tracker", Toast.LENGTH_SHORT).show();
+                }
+
+            } else {
+                jobScheduler.cancelAll();
+                Toast.makeText(MainActivity.this, "Tracker stopped", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +43,26 @@ public class MainActivity extends AppCompatActivity {
 
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
-        viewPager = findViewById(R.id.viewpager);
+        ViewPager viewPager = findViewById(R.id.viewpager);
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
 
-        tabLayout = findViewById(R.id.tabs);
+        TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+        jobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+
+        Switch jobSwitch = findViewById(R.id.service_switch);
+        jobSwitch.setOnCheckedChangeListener(toggleJob);
+    }
+
+    JobInfo getJobInfo() {
+        ComponentName componentName = new ComponentName(MainActivity.this, TrackerService.class);
+
+        return new JobInfo.Builder(281192, componentName)
+//                        .setPeriodic(4 * 60 * 60 * 100)
+                .setPeriodic(30 * 60 * 100)
+                .setPersisted(true)
+                .build();
     }
 }
--- app/src/main/java/com/mr/j/TrackerService.java
+++ app/src/main/java/com/mr/j/TrackerService.java
@@ -1,15 +1,40 @@
 package com.mr.j;
 
+import android.app.NotificationManager;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Handler;
+import android.os.PersistableBundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class TrackerService extends JobService {
 
+    Handler handler;
+    Runnable runnable;
+    String userId;
+
     @Override
     public boolean onStartJob(JobParameters params) {
-        //Todo do everything that the service was supposed to do.
-        //Todo use this link https://blog.teamtreehouse.com/scheduling-work-jobscheduler
-        return false;
+        PersistableBundle bundle = params.getExtras();
+        userId = bundle.getString(Constants.userIdKey);
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                getFollowerCount();
+            }
+        };
+
+        handler = new Handler();
+        handler.postDelayed(runnable, 10 * 1000);
+        return true;
     }
 
     @Override
@@ -17,4 +42,48 @@ public class TrackerService extends JobService {
         //Todo Think how to use this method. This method will be called by the Android system when the job is stopped in the mid.
         return false;
     }
+
+    private void getFollowerCount() {
+        GitHubAPICaller caller = new GitHubAPICaller(getBaseContext());
+
+        caller.getResponse(String.format(Constants.userUrl, userId), new VolleyCallback() {
+            @Override
+            public void onSuccessResponse(JSONArray result) {
+                try {
+                    JSONObject userObj = result.getJSONObject(0);
+                    int count = userObj.getInt(Constants.getFollowersCountKey);
+
+                    NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getBaseContext());
+                    mBuilder.setSmallIcon(R.drawable.ic_launcher_foreground);
+                    mBuilder.setContentTitle("Notification Alert");
+                    mBuilder.setContentText("Hi, You have " + count + " followers on GitHub");
+
+                    NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+                    // notificationID allows you to update the notification later on.
+                    if (mNotificationManager != null) {
+                        mNotificationManager.notify(123, mBuilder.build());
+                    }
+
+
+                    //Todo if count < or > localCount, update local count and check what changed.
+                    //Todo If changed, send a broadcast to a broadcast to check compare the saved and online list and generate notification of the user who un-followed
+
+                } catch (JSONException ex) {
+                    Log.e("TrackerService", "Error in JSON array response");
+                }
+            }
+
+            @Override
+            public void onErrorResponse(String error) {
+
+            }
+
+            @Override
+            public void onImageResponse(Bitmap image) {
+
+            }
+        });
+    }
+
 }
