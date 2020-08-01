e45c3be4aa74f1d750150c8c60dd97968fd8efce
Kshitij Saxena <kshtj246@outlook.com>
2020-08-01 23:57:20 +0530

    created shared header

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import Header from "./shared/Header";
 
+const pages = [
+  { Name: "I Follow", Path: "/ifollow" },
+  { Name: "Follow Me", Path: "/followme" },
+  { Name: "Following Delta", Path: "/delta" }
+];
+
 class App extends Component {
-  navigationItems = ['Follower Tracker', 'I Follow', 'Follow Me', "Doesn't Follow Me"];
+  //navigationItems = ['Follower Tracker', 'I Follow', 'Follow Me', "Doesn't Follow Me"];
 
   render() {
     return (
       <div>
-        <Header dark={true}>{this.navigationItems}</Header>
+        <Header dark={true}>Follower Tracker</Header>
       </div>
     );
   }
--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Header = ({ dark, children, className }) => {
+const Header = ({ dark, children, className, pages }) => {
   dark = !!dark ? "dark" : "light";
   return (
     <nav
@@ -9,7 +9,21 @@ const Header = ({ dark, children, className }) => {
         (className ? " " + className : "")
       }
     >
-      <span className="navbar-brand">{children}</span>      
+      <span className="navbar-brand">{children}</span>
+
+      {pages && pages.length > 0 && (
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            {pages.map((item, key) => (
+              <li className="nav-item" key={key}>
+                <a href={item.Path} className="nav-link">
+                  {item.Name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
