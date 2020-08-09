fa93da362876b8607c450d27840fd9c43d849439
Kshitij Saxena <kshtj246@outlook.com>
2020-08-09 20:35:41 +0530

    Updates

---
+++ precache-manifest.2fd59378722e394e3132b305dba36573.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90111df24190d71f2fd6b40e32007258",
+    "url": "/follower-tracker/index.html"
+  },
+  {
+    "revision": "dfcd10e2565ad1c352b6",
+    "url": "/follower-tracker/static/js/2.b1cdea93.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/follower-tracker/static/js/2.b1cdea93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66b9bcee02fc58f1557c",
+    "url": "/follower-tracker/static/js/main.4855bea3.chunk.js"
+  },
+  {
+    "revision": "5cc253df18fb020e7127",
+    "url": "/follower-tracker/static/js/runtime-main.1f823e9a.js"
+  }
+]);
